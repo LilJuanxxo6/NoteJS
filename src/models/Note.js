f6fcdfa5be51a0a5edcdcaf48613777b78bd940b
Juan Fernando <wuanter.castano@gmail.com>
2025-04-05 21:00:42 -0500

    Se empezo la parte del crud, actualmente se puede almacenar en la base de datos las notes, con fecha y acceder a la base de datos

---
+++ src/models/Note.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose'); // Importar mongoose para la conexión a la base de datos
+const { Schema } = mongoose; // Desestructurar el objeto Schema de mongoose
+
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now // Establecer la fecha por defecto como la fecha actual
+    }
+});
+
+module.exports = mongoose.model('Note', NoteSchema); // Exportar el modelo de nota
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,5 +1,33 @@
 const router = require('express').Router(); // Importar el módulo express
 
+const Note = require('../models/Note'); // Importar el modelo de nota
+
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note'); // Renderizar la vista de agregar nota
+});
+
+router.post('/notes/new-note', async (req, res) => {
+    const { title, description } = req.body;
+    const errors = []; // Inicializar un array de errores
+    if(!title){
+        errors.push({text: 'Por favor ingrese un título'}); // Agregar error si no hay título
+    }
+    if(!description){
+        errors.push({text: 'Por favor ingrese una descripción'}); // Agregar error si no hay descripción
+    }
+    if(errors.length > 0){ // Si hay errores
+        res.render('notes/new-note', { // Renderizar la vista de agregar nota con errores
+            errors,
+            title,
+            description
+        });
+    } else {
+        const newNote = new Note({ title, description }); // Crear una nueva nota
+        await newNote.save(); // Guardar la nueva nota en la base de datos AWAIT se usa para esperar la respuesta por ser petición asincrona
+        res.redirect('/notes'); // Redirigir a la ruta de notas
+    }
+});
+
 router.get('/notes', (req, res) => {
     res.send('Notas desde DB') // Ruta de las notas
 });
